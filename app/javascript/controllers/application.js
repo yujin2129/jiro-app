b1192e268dcb9e2847d1a268fd946a2ce5f35509
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-07-09 00:23:05 +0900

    Importmap構成を整理し、不要なビルドツールの設定を削除

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,7 @@
+import "@hotwired/turbo-rails"
 import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-loading"
 
 const application = Application.start()
-
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
-export { application }
+const context = definitionsFromContext(require.context("./controllers", true, /\.js$/))
+application.load(context)
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.16
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap" # @5.3.7
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
-pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.13
-pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.0.200
