e8a44e83a7a9b33885f87fc23cd991165377aef9
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-07-09 00:53:54 +0900

    StimulusをImportmap対応の構成に修正（Rails推奨の分割形式を採用）

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,7 +1,3 @@
-import "@hotwired/turbo-rails"
 import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-loading"
 
-const application = Application.start()
-const context = definitionsFromContext(require.context("./controllers", true, /\.js$/))
-application.load(context)
+export const application = Application.start()
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,5 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
+import { application } from "./application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+
 eagerLoadControllersFrom("controllers", application)
