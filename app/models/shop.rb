009d4623c5d990b62adb87edcee6c9ad677152d4
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-07-09 23:36:47 +0900

    Shopモデルにバリデーションを追加

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,7 @@
 class Shop < ApplicationRecord
   has_one_attached :image
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :address, presence: true, length: { maximum: 100 }
+  validates :rules, length: { maximum: 300 }
 end
