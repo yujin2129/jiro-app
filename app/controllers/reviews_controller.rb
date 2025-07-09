c294d093e33d8a220bc8813c5f855cbd6ca9e483
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-07-10 00:56:05 +0900

    Reviewコントローラを実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,35 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_shop
+
+  def create
+    @review = @shop.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to shop_path(@shop), notice: "レビューを投稿しました"
+    else
+      @reviews = @shop.reviews.includes(:user)
+      render "shops/show", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review = @shop.reviews.find(params[:id])
+    if @review.user == current_user
+      @review.destroy
+      redirect_to shop_path(@shop), notice: "レビューを削除しました"
+    else
+      redirect_to shop_path(@shop), alert: "削除できません"
+    end
+  end
+
+  private
+
+  def set_shop
+    @shop = Shop.find(params[:shop_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
