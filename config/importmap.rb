4e9f6999f25d651a462039e4ee76327dc13393ab
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-07-12 17:56:04 +0900

    お気に入り機能のTurbo Stream非同期処理を修正し、表示更新が反映されるよう修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js", preload: true
+pin "@hotwired/turbo", to: "@hotwired--turbo.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
