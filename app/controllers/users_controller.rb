e5bf3b9f576d33d8014c7d9a2a51aff5fe093e7e
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-07-13 22:11:56 +0900

    ユーザープロフィール画面にレビューとお気に入り店舗一覧を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
+    @reviews = @user.reviews.includes(:shop).order(created_at: :desc)
+    @favorite_shops = @user.favorite_shops.order(:name)
   end
 end
