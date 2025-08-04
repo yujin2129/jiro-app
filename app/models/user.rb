e0ae559d334614762a9c84993788f57b8af7dcaf
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-08-05 07:17:16 +0900

    Userモデルに管理者フラグを追加し、メソッドを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 20 }
+
+  def admin?
+    admin
+  end
 end
