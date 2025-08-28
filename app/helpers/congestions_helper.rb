d68317bda428f6583812b0cf145923b94c430a0b
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-08-29 01:21:34 +0900

    混雑情報タブのUIを強化

--- app/helpers/congestions_helper.rb
+++ app/helpers/congestions_helper.rb
@@ -1,2 +1,11 @@
 module CongestionsHelper
+  HIGHLIGHT_COLOR = "#ffd699"
+
+  CONGESTION_LEVEL_LABELS = {
+    1 => "空席あり",
+    2 => "満席近い",
+    3 => "〜30分待ち",
+    4 => "30分〜1時間待ち",
+    5 => "1時間〜待ち"
+  }
 end
