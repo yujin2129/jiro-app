26d2f62fdb84291d06fae9274da9898bc1760b8c
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-09-02 22:39:07 +0900

    Userモデルのバリデーションとadmin?メソッドのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション" do
+    it "名前、メール、パスワードがあれば有効" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it "名前が必須" do
+      user = build(:user, name: nil)
+      expect(user).not_to be_valid
+    end
+
+    it "名前が20文字以内であること" do
+      user = build(:user, name: "a" * 21)
+      expect(user).not_to be_valid
+    end
+  end
+
+  describe "#admin?" do
+    it "admin が true の場合は true を返す" do
+      user = build(:user, admin: true)
+      expect(user.admin?).to eq true
+    end
+
+    it "admin が false の場合は false を返す" do
+      user = build(:user, admin: false)
+      expect(user.admin?).to eq false
+    end
+  end
 end
