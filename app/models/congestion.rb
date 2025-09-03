42e8d00797fad932d3695cbfd608e9ddde6fe78b
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-09-04 03:13:20 +0900

    CongestionモデルのVISITED_TIME_SLOTS定数をバリデーションより上に移動

--- app/models/congestion.rb
+++ app/models/congestion.rb
@@ -1,4 +1,11 @@
 class Congestion < ApplicationRecord
+  VISITED_TIME_SLOTS = [
+    "0:00〜2:00", "2:00〜4:00", "4:00〜6:00",
+    "6:00〜8:00", "8:00〜10:00", "10:00〜12:00",
+    "12:00〜14:00", "14:00〜16:00", "16:00〜18:00",
+    "18:00〜20:00", "20:00〜22:00", "22:00〜24:00"
+  ]
+
   belongs_to :shop
   belongs_to :user
 
@@ -6,10 +13,4 @@ class Congestion < ApplicationRecord
   validates :visited_time, inclusion: { in: VISITED_TIME_SLOTS }
   validates :level, inclusion: { in: 1..5 }
 
-  VISITED_TIME_SLOTS = [
-    "0:00〜2:00", "2:00〜4:00", "4:00〜6:00",
-    "6:00〜8:00", "8:00〜10:00", "10:00〜12:00",
-    "12:00〜14:00", "14:00〜16:00", "16:00〜18:00",
-    "18:00〜20:00", "20:00〜22:00", "22:00〜24:00"
-  ]
 end
