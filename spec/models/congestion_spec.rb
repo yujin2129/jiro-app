7392c4a45b1b98866ecf80daa51229ae191e6c52
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-09-04 03:17:14 +0900

    Congestionモデルのバリデーションと定数のテストを追加

--- spec/models/congestion_spec.rb
+++ spec/models/congestion_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Congestion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション" do
+    it "shop, user, visited_on, visited_time, level があれば有効" do
+      congestion = build(:congestion)
+      expect(congestion).to be_valid
+    end
+
+    it "visited_on が必須" do
+      congestion = build(:congestion, visited_on: nil)
+      expect(congestion).not_to be_valid
+    end
+
+    it "visited_time が必須" do
+      congestion = build(:congestion, visited_time: nil)
+      expect(congestion).not_to be_valid
+    end
+
+    it "level が必須" do
+      congestion = build(:congestion, level: nil)
+      expect(congestion).not_to be_valid
+    end
+
+    it "level は 1〜5 の範囲であること" do
+      congestion = build(:congestion, level: 6)
+      expect(congestion).not_to be_valid
+    end
+
+    it "user が必須" do
+      congestion = build(:congestion, user: nil)
+      expect(congestion).not_to be_valid
+    end
+
+    it "shop が必須" do
+      congestion = build(:congestion, shop: nil)
+      expect(congestion).not_to be_valid
+    end
+  end
+
+  describe "定数 VISITED_TIME_SLOTS" do
+    it "定数に含まれない visited_time は無効" do
+      congestion = build(:congestion, visited_time: "25:00〜27:00")
+      expect(congestion).not_to be_valid
+    end
+  end
 end
