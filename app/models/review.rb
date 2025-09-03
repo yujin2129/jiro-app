27a6cdf77057fcb02194d3f76a22f47f7d5e5c61
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-09-03 22:31:20 +0900

    Reviewモデルにcontentの文字数制限のバリデーションを追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,6 @@ class Review < ApplicationRecord
 
   has_many_attached :images
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 500 }
   validates :rating, presence: true, inclusion: { in: 1..5 }
 end
