ab277fd52641d53baadfbaf743d937f8dc354f6c
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-09-03 22:39:55 +0900

    Favoriteモデルのバリデーションとアソシエーションのテストを追加

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション" do
+    it "user, shop があれば有効" do
+      favorite = build(:favorite)
+      expect(favorite).to be_valid
+    end
+
+    it "同じユーザーが同じ店舗を2回お気に入りできない" do
+      user = create(:user)
+      shop = create(:shop)
+      create(:favorite, user: user, shop: shop)
+      duplicate = build(:favorite, user: user, shop: shop)
+
+      expect(duplicate).not_to be_valid
+      expect(duplicate.errors[:user_id]).to include("はすでに存在します")
+    end
+  end
+
+  describe "アソシエーション" do
+    it { should belong_to(:user) }
+    it { should belong_to(:shop) }
+  end
 end
