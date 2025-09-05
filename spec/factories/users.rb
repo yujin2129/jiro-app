f2efd7ed52bd21959391b6c9b5bb3d70547fe544
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-09-06 05:27:24 +0900

    userファクトリのパスワードを強化された値に変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name { "テストユーザー" }
     sequence(:email) { |n| "user#{n}@example.com" }
-    password { "password" }
-    password_confirmation { "password" }
+    password { "TestPassword123!" }
+    password_confirmation { "TestPassword123!" }
   end
 end
