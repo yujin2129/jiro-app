72f027c257fc2876a7c787dd0825086400704780
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-09-07 19:52:46 +0900

    マイページ（プロフィール表示・編集、投稿レビュー・お気に入り表示）のシステムテストを追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "ユーザーの認証機能", type: :system do
-  let!(:user) { create(:user, email: "test@example.com", password: "password") }
+  let!(:user) { create(:user) }
 
   describe "ユーザー登録" do
     it "成功する場合" do
@@ -52,16 +52,78 @@ RSpec.describe "ユーザーの認証機能", type: :system do
   end
 
   describe "ログアウト" do
-    it "成功する" do
+    before do
       login(user)
       expect(page).to have_content "ログインしました"
-      
+    end
+
+    it "成功する" do
       visit root_path
       click_on user.name
       click_button "ログアウト"
       
       expect(page).to have_content "ログアウトしました"
       expect(page).not_to have_content user.name
+    end
+  end
+
+  describe "マイページ（プロフィール）機能" do
+    let!(:review) { create(:review, user: user) }
+    let(:favorite_shop) { create(:shop) }
+    let!(:favorite) { create(:favorite, user: user, shop: favorite_shop) }
+  
+    before do
+      login(user)
+      expect(page).to have_content "ログインしました"
+      visit user_profile_path(user)
+    end
+  
+    it "プロフィール情報が表示される" do
+      expect(page).to have_content user.name
+      expect(page).to have_content user.email
+      expect(page).to have_link "プロフィールを編集"
+    end
+  
+    it "投稿したレビューが表示される" do
+      click_button "投稿レビュー"
+      expect(page).to have_content "投稿したレビュー"
+      expect(page).to have_content review.content
+    end
+  
+    it "お気に入り店舗が表示される" do
+      click_button "お気に入り店舗"
+      expect(page).to have_content "お気に入り店舗"
+      expect(page).to have_content favorite_shop.name
+    end
+  end
+
+  describe "マイページ（プロフィール編集）機能" do
+    before do
+      login(user)
+      expect(page).to have_content "ログインしました"
+      visit user_profile_path(user) # マイページ
+      click_link "プロフィールを編集"
+      expect(page).to have_content "プロフィール編集"
+    end
+  
+    it "名前とメールアドレスを編集できる" do
+      fill_in "名前", with: "新しい名前"
+      fill_in "Eメール", with: "new_email@example.com"
+      fill_in "現在のパスワード", with: user.password
+      click_button "更新する"
+  
+      expect(page).to have_content "アカウント情報を変更しました"
+      expect(current_path).to eq root_path
+    end
+  
+    it "必須項目が空だとエラーが表示される" do
+      fill_in "名前", with: ""
+      fill_in "Eメール", with: ""
+      fill_in "現在のパスワード", with: user.password
+      click_button "更新する"
+  
+      expect(page).to have_content "名前を入力してください"
+      expect(page).to have_content "Eメールを入力してください"
     end
   end
 end
