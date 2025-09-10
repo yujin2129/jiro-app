dd160fcdb28b47429c28a3d78fc4844c5cc357ed
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-09-10 11:12:48 +0900

    本番環境の 404 を解消するため controllers/application を importmap 経由でインポート

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,5 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "./application"
+import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
-eagerLoadControllersFrom("controllers", application)
+eagerLoadControllersFrom("controllers", application)    
