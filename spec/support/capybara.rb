13b99095a5ce7173ca078d7cf9ac251fde59c3b9
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-09-11 22:29:39 +0900

    CI環境向けにCapybaraをヘッドレスChromeで安定動作するよう設定
    - spec/support/capybara.rb を追加
    - Chrome起動オプション (--headless, --no-sandbox, --disable-dev-shm-usage など) を設定
    - 並列実行時の競合回避のため --user-data-dir をユニーク化
    - javascript_driver を :headless_chrome_ci に設定

---
+++ spec/support/capybara.rb
@@ -0,0 +1,15 @@
+require "capybara/rspec"
+
+Capybara.register_driver :headless_chrome_ci do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+  options.args << "--headless=new"
+  options.args << "--no-sandbox"
+  options.args << "--disable-dev-shm-usage"
+  options.args << "--disable-gpu"
+  options.args << "--window-size=1400,1400"
+  options.args << "--user-data-dir=/tmp/chrome-user-data-#{SecureRandom.hex}"
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
+end
+
+Capybara.javascript_driver = :headless_chrome_ci
