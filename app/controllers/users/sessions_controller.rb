6dc81631b7a409ba542595862cadd6ff5020e4c2
Yujin Yagi <yujinsoccer2129@icloud.com>
2025-10-06 22:06:19 +0900

    ゲストログイン機能のフラッシュメッセージをi18nファイルに追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,12 +2,12 @@ class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to root_path, notice: "ゲストユーザーとしてログインしました。"
+    redirect_to root_path, notice: t('flash.user.guest_login')
   end
 
   def guest_admin_sign_in
     user = User.guest_admin
     sign_in user
-    redirect_to root_path, notice: "管理者ゲストユーザーとしてログインしました。"
+    redirect_to root_path, notice: t('flash.user.guest_admin_login')
   end
 end
